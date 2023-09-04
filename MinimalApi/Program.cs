bab0559fec7d5a94fe9a1f9907767b01f74135d0
kallnascimento <iksn7@icloud.com>
2023-09-04 16:05:34 -0300

    Feat:Tasks Endpoints

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,30 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("Task
 
 var app = builder.Build();
 app.UseHttpsRedirection();
+app.MapDelete("/task/{id}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tasks.FindAsync(id) is Task task)
+    {
+        db.Tasks.Remove(task);
+        await db.SaveChangesAsync();
+        return Results.Ok(task);
+    }
+
+    return Results.NotFound("Esta tarefa não foi encontrada");
+});
 app.MapGet("/tasks", async (AppDbContext db) => await db.Tasks.ToListAsync());
+app.MapGet("/task/{id}", async (int id, AppDbContext db) =>
+    await db.Tasks.FindAsync(id) is Task task ? Results.Ok(task) : Results.NotFound());
+app.MapGet("/tasks/done", async (AppDbContext db) => await db.Tasks.Where(t => t.IsDone).ToListAsync());
+app.MapPut("tasks/{id}", async (int id, Task inputTask, AppDbContext db) =>
+{
+    var task = await db.Tasks.FindAsync(id);
+    if (task is null) return Results.NotFound();
+    task.Name = inputTask.Name;
+    task.IsDone = inputTask.IsDone;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 app.MapPost("/tasks", async (Task task, AppDbContext db) =>
 {
     db.Tasks.Add(task);
@@ -27,7 +51,7 @@ class Task
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public bool isDone { get; set; }
+    public bool IsDone { get; set; }
 }
 
 class AppDbContext : DbContext
